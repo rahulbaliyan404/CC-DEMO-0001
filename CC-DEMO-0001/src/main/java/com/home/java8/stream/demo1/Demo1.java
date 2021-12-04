67f5780921549ff1e18d06914e38f4a79225f06a
rahulbaliyan404 <rahulbaliyan404@gmail.com>
2021-12-04 18:55:08 +0530

    This is second commit.

---
+++ CC-DEMO-0001/src/main/java/com/home/java8/stream/demo1/Demo1.java
@@ -0,0 +1,20 @@
+package com.home.java8.stream.demo1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo1 {
+	List<Integer> getData() {
+		List<Integer> al = new ArrayList<Integer>();
+		al.add(10);
+		al.add(15);
+		al.add(30);
+		al.add(20);
+		al.add(15);
+		al.add(50);
+		al.add(5);
+		al.add(40);
+		return al;
+	}
+
+}
---
+++ CC-DEMO-0001/src/test/java/com/home/java8/stream/demo1/Demo1Test.java
@@ -0,0 +1,12 @@
+package com.home.java8.stream.demo1;
+
+import org.junit.jupiter.api.Test;
+
+public class Demo1Test {
+
+	@Test
+	public void getData() {
+		System.out.println(" Junit Testing class.");
+	}
+
+}
