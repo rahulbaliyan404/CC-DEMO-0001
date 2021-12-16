20aca87263dccdd144b729f9fb11b8c2397aec22
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2021-12-16 17:23:06 +0530

    This is second commit 16-12-2021.

--- CC-DEMO-0001/src/main/java/com/home/java8/lambda/Demo.java
+++ CC-DEMO-0001/src/main/java/com/home/java8/lambda/Demo.java
@@ -1,5 +1,9 @@
 package com.home.java8.lambda;
 
 public class Demo {
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
--- CC-DEMO-0001/src/main/java/com/home/java8/stream/demo2/Demo2.java
+++ CC-DEMO-0001/src/main/java/com/home/java8/stream/demo2/Demo2.java
@@ -1,5 +1,9 @@
 package com.home.java8.stream.demo2;
 
 public class Demo2 {
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
