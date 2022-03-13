2238edc95b6f46e36f673d0ef7fdd0fc371e5c10
rahulbaliyan404 <rahulbaliyan404@gmail.com>
2022-03-13 18:03:41 +0530

    First commit.

---
+++ CC-DEMO-0001/src/main/java/com/home/java8/default_method/demo1/Demo2.java
@@ -0,0 +1,8 @@
+package com.home.java8.default_method.demo1;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		
+	}
+
+}
--- CC-DEMO-0001/src/main/java/com/home/java8/default_method/demo2/Demo2.java
+++ CC-DEMO-0001/src/main/java/com/home/java8/default_method/demo2/Demo2.java
@@ -1,5 +1,8 @@
 package com.home.java8.default_method.demo2;
 
 public class Demo2 {
+	public static void main(String[] args) {
+		System.out.println("Hello Testing.");
+	}
 
 }
